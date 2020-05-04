da3e9a0d62acb81dce444c60117d07ee571f7593
Oladiy <bd3396121@gmail.com>
2020-05-04 16:14:36 +0300

    Реализовал методы для работы с БД

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include "storage.cpp"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::map<string, string> result;
+    StorageMySQL< std::map<string,string> > database;
+    database.saveData("FourthFile", "1289", "2020-03-03");
+    database.saveData("FirstFile", "1289", "2020-03-03");
+    database.saveData("SixthFile", "1289", "2020-03-03");
+    database.saveData("SeventhFile", "1289", "2020-03-03");
+    result = database.getData("FirstFile");
+    cout << result["Key"] << endl;
+    cout << result["Password"] << endl;
+    cout << result["DeletionDate"] << endl;
+    result.clear();
+    result = database.getData("SeventhFile");
+    cout << result["Key"] << endl;
+    cout << result["Password"] << endl;
+    cout << result["DeletionDate"] << endl;
     return 0;
 }
--- test/storage_test.cpp
+++ test/storage_test.cpp
@@ -7,66 +7,12 @@ using ::testing::SetArgReferee;
 using ::testing::DoAll;
 using ::testing::Return;
 
-class MockDataBase: public IDBManager {
+/*class MockDataBase: public IDBManager {
 public:
     MOCK_METHOD4(saveData, int(std::string path, std::string link, std::string key, std::string date));
     MOCK_METHOD1(getData, std::string(std::string link));
     MOCK_METHOD1(deleteData, bool(std::string link));
 };
-
-TEST(Storage, getPathToFileTest) {
-    std::string path = "/usr/bin/file.txt";
-    std::string link = "google.com/download/file.txt";
-    std::string result = "someinfo";
-    auto* database = new MockDataBase();
-    EXPECT_CALL(*database,
-            getData(link))
-            .WillRepeatedly(Return(result));
-    auto* storage = new Storage<MockDataBase>(database);
-    EXPECT_EQ(storage->getPathToFile(link), path);
-    delete database;
-    delete storage;
-}
-
-TEST(Storage, isUniqueLink) {
-    std::string link = "google.com/download/file.txt";
-    std::string result = "someinfo";
-    auto* database = new MockDataBase();
-    EXPECT_CALL(*database,
-                getData(link))
-            .WillRepeatedly(Return(result));
-    auto* storage = new Storage<MockDataBase>(database);
-    EXPECT_TRUE(storage->isUniqueLink(link));
-    delete database;
-    delete storage;
-}
-
-TEST(Storage, isValidLink) {
-    std::string link = "google.com/download/file.txt";
-    std::string result = "someinfo";
-    auto* database = new MockDataBase();
-    EXPECT_CALL(*database,
-                getData(link))
-            .WillRepeatedly(Return(result));
-    auto* storage = new Storage<MockDataBase>(database);
-    EXPECT_TRUE(storage->isValidLink(link));
-    delete database;
-    delete storage;
-}
-
-TEST(Storage, isCorrectKey) {
-    std::string link = "google.com/download/file.txt";
-    std::string key = "key1337";
-    std::string result = "someinfo";
-    auto* database = new MockDataBase();
-    EXPECT_CALL(*database,
-                getData(link))
-            .WillRepeatedly(Return(result));
-    auto* storage = new Storage<MockDataBase>(database);
-    EXPECT_TRUE(storage->isCorrectKey(link, key));
-    delete database;
-    delete storage;
-}
 
 TEST(StorageDateInspector, checkDeletionDate) {
     std::string link = "google.com/download/file.txt";
@@ -79,4 +25,4 @@ TEST(StorageDateInspector, checkDeletionDate) {
     EXPECT_EQ(storageDateInspector->checkDeletionDate(link), SUCCESS);
     delete storageDateInspector;
     delete database;
-}
+} */
